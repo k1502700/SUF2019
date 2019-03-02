f901c20b3381e998ea50d5a6ebcd8b2202bd8930
Robert Simola <k1502700@kcl.ac.uk>
2019-03-02 21:18:52 +0000

    add sidebar, fix table

--- src/View/BondTableView.java
+++ src/View/BondTableView.java
@@ -4,13 +4,13 @@ import Model.DisplayBond;
 import javafx.beans.NamedArg;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.scene.Group;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
+import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
@@ -81,7 +81,33 @@ public class BondTableView extends Scene {
          bp.setCenter(bondtable);
         bp.setTop(title);
 
+        Sidebar sd = new Sidebar();
+        bp.setRight(sd);
+
+        bondtable.getSelectionModel().setSelectionMode(
+                SelectionMode.MULTIPLE
+        );
+
+        Button button2 = new Button("GetSelection");
+
+        button2.setOnAction(new EventHandler<ActionEvent>() {
+            @Override public void handle(ActionEvent e) {
+
+                ObservableList selectedItems = bondtable.getSelectionModel().getSelectedItems();
+
+                for (int i = 0; i < selectedItems.size(); i++) {
+                   DisplayBond displayBond = (DisplayBond) selectedItems.get(i);
+                    System.out.println(displayBond.getName());
+                }
+
+            }
+        });
+
+        bp.setBottom(button2);
+
+
         ((Group) this.getRoot()).getChildren().addAll(bp);
+
 
 
     }
